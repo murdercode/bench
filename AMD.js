6e5c95245ec7c16fec0837e34110b859e600514f
Stefano Novelli <murdercode@gmail.com>
2019-09-11 13:05:52 +0200

    Create AMD.js

---
+++ AMD.js
@@ -0,0 +1,35 @@
+// ==UserScript==
+// @name         CPU AMD
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        https://www.amd.com/*
+// @grant        none
+// @require http://code.jquery.com/jquery-3.4.1.min.js
+
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    var totalO = "";
+
+    $( "#product-specs .field--label-inline" ).each(function( index ) {
+        var title= $(this).find(".field__label").text().replace(/ +(?= )/g,'');
+        title = $.trim(title);
+        var desc = $(this).find(".field__item").text().replace(/ +(?= )/g,'');
+        desc = $.trim(desc);
+        var output = title + "::" + desc + "\n";
+        totalO = totalO + output;
+});
+
+    //totalO = totalO.replace(/ +(?= )/g,'');
+    //totalO = totalO.replace(/[\r\n]{2,}/g, "\n");
+    //totalO = totalO.replace("\n::\n","::");
+
+
+$("body").prepend("<textarea rows=10 cols=100>"+totalO+"");
+
+    // Your code here...
+})();
