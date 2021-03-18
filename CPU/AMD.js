c23b20291a99b8f13ed74c4a81e1d49f61b86518
Stefano Novelli <murdercode@gmail.com>
2021-03-18 14:28:29 +0100

    Compatibile con nuovo Tom's Bench

--- CPU/AMD.js
+++ CPU/AMD.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         CPU AMD
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.2
 // @description  try to take over the world!
 // @author       You
 // @match        https://www.amd.com/*
@@ -14,22 +14,29 @@
     'use strict';
 
     var totalO = "";
+    var final = "";
 
     $( "#product-specs .field--label-inline" ).each(function( index ) {
         var title= $(this).find(".field__label").text().replace(/ +(?= )/g,'');
         title = $.trim(title);
         var desc = $(this).find(".field__item").text().replace(/ +(?= )/g,'');
         desc = $.trim(desc);
-        var output = title + "::" + desc + "\n";
+        var output = "{\""+title + "\":\"" + desc + "\"},";
         totalO = totalO + output;
 });
+
+    // Remove last ,
+    totalO = totalO.substring(0, totalO.length - 1);
+
+    final = "["+totalO+"]";
+
 
     //totalO = totalO.replace(/ +(?= )/g,'');
     //totalO = totalO.replace(/[\r\n]{2,}/g, "\n");
     //totalO = totalO.replace("\n::\n","::");
 
 
-$("body").prepend("<textarea rows=10 cols=100>"+totalO+"");
+$("body").prepend("<textarea rows=10 cols=100>"+final+"");
 
     // Your code here...
 })();
